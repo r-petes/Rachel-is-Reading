3f20800a08c64cb09d06350ad2786178ef8272e8
r-petes <rachel.peterson.5683@gmail.com>
2021-09-01 22:17:35 -0600

    updated links to work correctly

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import './App.css';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
+import { BrowserRouter, Route } from 'react-router-dom'; 
+import About from './components/About';
+import Blogs from './components/Blogs';
+import Books from './components/Books';
 
 
 function App() {
   return (
+    <BrowserRouter>
+
+      <Route exact path="/" component={Blogs} />
+        <Route path="/about" component={About} />
+        <Route path="/books" component={Books} />
+
     <Navbar className="navbar">
     <Container>
     <Navbar.Brand href="#home" id="title">RACHEL IS READING</Navbar.Brand>
     <Nav className="me-auto">
         <Nav.Link href="/" id="blog">blog</Nav.Link>
         <Nav.Link href="books" id="books">books</Nav.Link>
-        <Nav.Link href="about" id="about">about</Nav.Link>      
+        <Nav.Link href="/about" id="about">about</Nav.Link>      
     </Nav>
     </Container>
   </Navbar>
+  </BrowserRouter>
     )
 }
 
