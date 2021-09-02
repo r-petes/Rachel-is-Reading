ad34d37eeb038b99ef625ef8f58683818299b09e
r-petes <rachel.peterson.5683@gmail.com>
2021-09-01 20:11:11 -0600

    initial commit, basic react app created

---
+++ src/components/Button.js
@@ -0,0 +1,13 @@
+const Button = ({ color, text }) => {
+    return (
+        <button style={{backgroundColor: color }} className='btn'>{text}</button>
+    )
+} 
+
+
+Button.defaultProps = {
+    color: 'steelblue'
+}
+
+
+export default Button
