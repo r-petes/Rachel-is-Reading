bc2a810ef0688d3db688205af46e9000f41f8e47
r-petes <rachel.peterson.5683@gmail.com>
2021-09-01 21:14:49 -0600

    updated branding (colors, fonts, etc.) of navbar

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ import './App.css';
 function App() {
   return (
     <>
-    <Navbar bg="light" variant="light">
+    <Navbar className="navigationbar">
     <Container>
-    <Navbar.Brand href="#home">Rachel is Reading</Navbar.Brand>
+    <Navbar.Brand href="#home" id="title">RACHEL IS READING</Navbar.Brand>
     <Nav className="me-auto">
-      <Nav.Link href="#Blog">Blog</Nav.Link>
-      <Nav.Link href="#Books">Books</Nav.Link>
-      <Nav.Link href="#About">About</Nav.Link>
+      <Nav.Link href="#Blog" id="blog">blog</Nav.Link>
+      <Nav.Link href="#Books" id="books">books</Nav.Link>
+      <Nav.Link href="#About" id="pages">about</Nav.Link>
     </Nav>
     </Container>
   </Navbar>
