165c82dd6276a4a94412d904504fd86675a2537d
r-petes <rachel.peterson.5683@gmail.com>
2021-09-01 21:03:07 -0600

    Included Bootstrap and Navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import logo from './logo.png';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 import './App.css';
 
 
-// insert navbar CSS here
-
 function App() {
   return (
-    <div> 
-    <div>
-      <img src={logo} alt="Logo" />
-      <div>
-        <div>Books</div>
-        <div>Blogs</div>
-        <div>About</div>
-      </div>
-    </div>
-  </div>
+    <>
+    <Navbar bg="light" variant="light">
+    <Container>
+    <Navbar.Brand href="#home">Rachel is Reading</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#Blog">Blog</Nav.Link>
+      <Nav.Link href="#Books">Books</Nav.Link>
+      <Nav.Link href="#About">About</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+  </>
   )
 }
 
